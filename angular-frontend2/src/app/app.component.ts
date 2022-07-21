9eccbf5a26ae0797c1ea576e57c02b9f3faaf8c0
kmikosxpanxion <106502570+kmikosxpanxion@users.noreply.github.com>
2022-07-21 14:37:38 -0400

    Add files via upload
    Added angular-frontend

---
+++ angular-frontend2/src/app/app.component.ts
@@ -0,0 +1,30 @@
+
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './login/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  implements OnInit {
+
+  title = 'Angular + Spring Boot CRUD Full Stack App';
+  isLoggedIn = false;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService) { }
+
+  ngOnInit() {
+    this.isLoggedIn = this.authenticationService.isUserLoggedIn();
+    console.log('menu ->' + this.isLoggedIn);
+  }
+
+  handleLogout() {
+    this.authenticationService.logout();
+  }
+  
+}
